ca32fa41334c3a63ab23b0f8c40d020cbb8ee562
Billy Bogardus <billbogardus1@gmail.com>
2019-11-25 20:23:02 -0700

    Minor Changes

--- Code/LM75BTemp.c
+++ Code/LM75BTemp.c
@@ -4,7 +4,16 @@
  *
  * Created on October 21, 2019, 5:00 PM
  */
-#include <xc.h>
 #include "LM75BTemp.h"
 
+
+signed int LM758BintoInt(signed int Raw, bool CorF = 1)
+{
+    signed int s = Raw & 0b1000000000000000;
+    signed int temp = Raw & 0x7F;
+    temp = temp >> 5;
+    temp = temp | s;
+    return temp/8;
+    
+}
 
